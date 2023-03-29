214cb393b9cf82661cb483d92c3ce880823b4a68
tamzid620 <tamzid620@gmail.com>
2023-03-29 20:56:11 +0600

    quantity added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -6,9 +6,14 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0; 
     let totalShipping =0;
+    let quantity =0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping+ product.shipping
+        // if(product.qunatity === 0){
+        //     product.qunatity = 1;
+        // }
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice *7/100;
     const grandTotal = totalPrice + totalShipping + tax;
@@ -16,7 +21,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: $ {totalPrice.toFixed(2)}</p>
             <p>Total Charge:${totalShipping.toFixed(2)} </p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -18,13 +18,24 @@ const Shop = () => {
 
     // button cart ---------------------
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product]
+        let newCart = [];
+        // const newCart = [...cart, product]
+        const exists = cart.find(pd => pd.id === product.id);
+    if(!exists){
+        product.quantity = 1;
+        newCart = [...cart, product];
+    }else{
+        exists.quantity = exists.quantity + 1;
+        const remaining = cart.filter(pd => pd.id !== product.id);
+        newCart = [...remaining, exists]
+    }
         setCart(newCart); 
         addToDb(product.id)
     }
 
     useEffect(()=> {
         const storedCart = getShoppingCart();
+        const savedCart = [];
         for(const id in storedCart){
             const addedProduct = products.find( product => product.id === id)
             if(addedProduct){
