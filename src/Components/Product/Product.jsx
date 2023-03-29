f5c7b1a85f8ec70aa0222cbc6ded6329af4ee5f7
tamzid620 <tamzid620@gmail.com>
2023-03-29 17:24:48 +0600

    third commit

---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const {img, name, price, seller, ratings, qunatity} = props.product
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product.info'>
+            <h3 className='product-name'>{name}</h3>
+            <h2 className='product-price'>Price: {price}$</h2>
+            <h5 className='product-seller'>Manufacturer: {seller}</h5>
+            <p>Rating: {ratings} star</p>
+            </div>
+            <button className='btn-cart'>ADD TO CART</button>
+            
+        </div>
+    );
+};
+
+export default Product;
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
@@ -14,10 +16,12 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className='products-container'>
-            <h2> Total Products :{products.length}</h2>
+                {
+                    products.map(product => <Product key ={product.id} product ={product}></Product>)
+                }
             </div>
             <div className='cart-container'>
-            <h2>Order Summary</h2>
+           <Cart></Cart>
             </div>
         </div>
     );
